98ec7bdaa9177aecddea43d7d91dc73cf025e655
Anelise <ane.schunk@gmail.com>
2019-04-16 17:18:31 -0300

    do trabalho 2

--- criandoBanco.sql
+++ criandoBanco.sql
@@ -8,6 +8,6 @@ create table usuarios(
 	idade int
 );
 
-insert into usuarios(nome, email, idade) values(
- "Victor Lima", "email@teste.com",8
-);
+insert into usuarios(nome, email, idade) values( "Victor Lima", "email@teste.com",8);
+insert into usuarios(nome, email, idade) values( "Luis Silva", "luissilva@teste.com",30);
+insert into usuarios(nome, email, idade) values( "Maria", "maria@teste.com",10);

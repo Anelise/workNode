0264b9b7518a9b3e181b1b5b455006c54be22c9e
Anelise <ane.schunk@gmail.com>
2019-04-15 21:20:03 -0300

    comit2

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log("hellow")
+const express = require('express');
+const app = express();
+
+app.get("/",function(req,res){
+	res.send("Página Principal!");
+});
+
+app.get("/sobre",function(req,res){
+	res.send("página sobre a empresa!");
+});
+
+app.get("/contato",function(req,res){
+	res.send("Me contate pelo teste@teste");
+});
+
+app.get("/blog/:nome/:cargo",function(req,res){
+	res.send("Pagina de blog"+req.params.nome+req.params.cargo);
+});
+
+
+app.listen(8081	, function(){console.log("Servidor rodando")});

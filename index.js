0baeaa5a5a67cd3b8c712fd162d30aec2fdf1127
Anelise <ane.schunk@gmail.com>
2019-04-18 11:01:24 -0300

    tttt

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const app = express();
 
+const handlebars = require("express-handlebars");
+
+const Sequelize = require('sequelize');
+const sequelizeInstance	=  new Sequelize('sistemanodejs','root','root',{host:'localhost',dialect:'mysql'});
+
+app.engine('handlebars', handlebars({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
 app.get("/",function(req,res){
 	res.sendFile(__dirname+"/html/primeiro.html");
 });
@@ -14,8 +22,8 @@ app.get("/contato",function(req,res){
 });
 
 app.get("/blog/:nome/:cargo",function(req,res){
-	res.send("Pagina de blog"+req.params.nome+req.params.cargo);ççççççç
+	res.send("Pagina de blog"+req.params.nome+req.params.cargo);
 });
 
 
-app.listen(8081	, function(){console.log("Servidor rodando")});cdccdddcdcdc
+app.listen(8081	, function(){console.log("Servidor rodando")});

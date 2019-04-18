7c755a2f5b851c504846adf0c78dccf1a9152c86
Anelise <ane.schunk@gmail.com>
2019-04-18 11:44:26 -0300

    ola

--- index.js
+++ index.js
@@ -10,15 +10,26 @@ app.engine('handlebars', handlebars({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
 app.get("/",function(req,res){
-	res.sendFile(__dirname+"/html/primeiro.html");
+	//res.sendFile(__dirname+"/html/primeiro.html");
+	res.render('primeiro');
+});
+
+app.get("/cad",function(req,res){
+	res.render('formulario');
+});
+
+app.post("/add",function(req,res){
+	res.send('Formulário Recebido!');
 });
 
 app.get("/sobre",function(req,res){
-	res.sendFile(__dirname+"/html/sobre.html");
+	//res.sendFile(__dirname+"/html/sobre.html");
+	res.render('primeiro');
 });
 
 app.get("/contato",function(req,res){
-	res.sendFile(__dirname+"/html/contato.html");
+	//res.sendFile(__dirname+"/html/contato.html");
+	res.render('contato');
 });
 
 app.get("/blog/:nome/:cargo",function(req,res){
